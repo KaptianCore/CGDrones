bb8c5db0368df1d3511c0b17f79dd82fd5c796e8
Kaptian Core <kaptiancore@gmail.com>
2021-08-12 16:37:55 +1000

    Add Back Invis Weapon

--- lua/entities/ausdrone_spy/shared.lua
+++ lua/entities/ausdrone_spy/shared.lua
@@ -4,75 +4,71 @@ ENT.PrintName = "AUS Drone"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
 ENT.Category = "CG Drones"
-
 ENT.UNIT = "AUS"
-
 ENT.HUD_hudName = "Camera"
-
 ENT.Model = "models/dronesrewrite/spydr/spydr.mdl"
-
 ENT.Weight = 90
-
 ENT.Speed = 800
 ENT.UpSpeed = 3300
 ENT.RotateSpeed = 5
 ENT.AngOffset = 4
 ENT.NoiseCoefficient = 0.5
 ENT.Alignment = 3
-
 ENT.Damping = 1
 ENT.AngDamping = 0
 ENT.AngPitchDamping = 1
 ENT.AngYawDamping = 0.4
 ENT.AngRollDamping = 1
-
 ENT.HackValue = 3
-
 ENT.FirstPersonCam_pos = Vector(20.5, 0, 0)
 ENT.ThirdPersonCam_distance = 50
 ENT.RenderCam = false
-
 ENT.AllowYawRestrictions = true
 ENT.YawMin = -80
 ENT.YawMax = 80
-
 ENT.KeysFuncs = DRONES_REWRITE.DefaultKeys()
-
 ENT.AllowPitchRestrictions = false
-
 ENT.HealthAmount = 250
 ENT.DefaultHealth = 250
-
 ENT.Fuel = 200
 ENT.MaxFuel = 125
 ENT.FuelReduction = 0.1
 
 ENT.Sounds = {
-	PropellerSound = {
-		Name = "npc/manhack/mh_engine_loop1.wav",
-		Pitch = 200,
-		Volume = 0.005,
-		Level = 65
-	},
-
-	ExplosionSound = {
-		Name = "ambient/explosions/explode_1.wav",
-		Level = 69
-	}
+    PropellerSound = {
+        Name = "npc/manhack/mh_engine_loop1.wav",
+        Pitch = 200,
+        Volume = 0.005,
+        Level = 65
+    },
+    ExplosionSound = {
+        Name = "ambient/explosions/explode_1.wav",
+        Level = 69
+    }
 }
 
 ENT.Propellers = {
-	Damage = 1,
-	Scale = 1.3,
-	Health = 30,
-	HitRange = 14,
-	Model = "models/dronesrewrite/propellers/propeller1_2.mdl",
-
-	Info = {
-		Vector(0, 0, 1)
-	}
+    Damage = 1,
+    Scale = 1.3,
+    Health = 30,
+    HitRange = 14,
+    Model = "models/dronesrewrite/propellers/propeller1_2.mdl",
+    Info = {Vector(0, 0, 1)}
 }
 
-ENT.Weapons = {["Camera"] = { Name = "Camera" }}
+ENT.Weapons = {
+    ["User & Invisible"] = {
+        Name = "User",
+        Sync = {
+            ["1"] = {
+                fire1 = "fire2"
+            }
+        }
+    },
+    ["1"] = {
+        Name = "Invisible",
+        Select = false
+    }
+}
 
 ENT.Modules = DRONES_REWRITE.GetBaseModules()
